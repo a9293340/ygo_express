1da049f3915e3c646dac1c19be9830c7b3b09735
Eric Hong <f102041332@gmail.com>
2023-04-12 23:59:40 +0800

    [feature] cards_image

--- routes/backend/cardsImage.js
+++ routes/backend/cardsImage.js
@@ -1,4 +1,25 @@
-const express = require('express');
+const express = require("express");
+const { limiter } = require("../../config/tools/rate-limiter");
+const { checkToken, decryptRes } = require("../../config/tools/encryptNToken");
+const {
+  canNotBeSameBeforeAdd,
+  pList,
+} = require("../../config/tools/postAction");
+const { articleEdit } = require("../../config/tools/articleApi");
 const router = express.Router();
+
+router.post("/add", limiter, checkToken, async (req, res, next) => {
+  const { token, tokenReq, ...useful } = decryptRes(req.body.data);
+  await canNotBeSameBeforeAdd(res, next, "cards_image", useful, "number");
+});
+
+router.post("/list", limiter, checkToken, (req, res, next) => {
+  const { filter } = decryptRes(req.body.data);
+  pList(res, next, "cards_image", filter, false, false);
+});
+
+router.post("/edit", limiter, checkToken, async (req, res, next) => {
+  await articleEdit(req, res, next, "cards_image");
+});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,35 @@
-const adminRouter = require('./backend/admin');
-const serialIntroductionRouter = require('./backend/serialIntroduction');
-const usefulCardIntroductionRouter = require('./backend/usefulCardIntroduction');
-const metaDeckRouter = require('./backend/metaDeck');
-const productInformationRouter = require('./backend/productInformation');
-const rulesRouter = require('./backend/rules');
-const seriesStoryRouter = require('./backend/seriesStory');
-const battlePaperRouter = require('./backend/battlePaper');
-const cardsRouter = require('./backend/cards');
-const productionInformationTypeRouter = require('./backend/productionInformationType');
-const calendarRouter = require('./backend/calendar');
-const tagRouter = require('./backend/tag');
-const bannerRouter = require('./backend/banner');
-const packTypeRouter = require('./backend/packType');
-const permissionRouter = require('./backend/permission');
-const cardsImageRouter = require('./backend/cardsImage');
+const adminRouter = require("./backend/admin");
+const serialIntroductionRouter = require("./backend/serialIntroduction");
+const usefulCardIntroductionRouter = require("./backend/usefulCardIntroduction");
+const metaDeckRouter = require("./backend/metaDeck");
+const productInformationRouter = require("./backend/productInformation");
+const rulesRouter = require("./backend/rules");
+const seriesStoryRouter = require("./backend/seriesStory");
+const battlePaperRouter = require("./backend/battlePaper");
+const cardsRouter = require("./backend/cards");
+const productionInformationTypeRouter = require("./backend/productionInformationType");
+const calendarRouter = require("./backend/calendar");
+const tagRouter = require("./backend/tag");
+const bannerRouter = require("./backend/banner");
+const packTypeRouter = require("./backend/packType");
+const permissionRouter = require("./backend/permission");
+const cardsImageRouter = require("./backend/cardsImage");
 
 module.exports = {
-	adminRouter,
-	serialIntroductionRouter,
-	usefulCardIntroductionRouter,
-	metaDeckRouter,
-	productInformationRouter,
-	rulesRouter,
-	seriesStoryRouter,
-	battlePaperRouter,
-	cardsRouter,
-	productionInformationTypeRouter,
-	calendarRouter,
-	tagRouter,
-	bannerRouter,
-	packTypeRouter,
-	permissionRouter,
-	cardsImageRouter,
+  adminRouter,
+  serialIntroductionRouter,
+  usefulCardIntroductionRouter,
+  metaDeckRouter,
+  productInformationRouter,
+  rulesRouter,
+  seriesStoryRouter,
+  battlePaperRouter,
+  cardsRouter,
+  productionInformationTypeRouter,
+  calendarRouter,
+  tagRouter,
+  bannerRouter,
+  packTypeRouter,
+  permissionRouter,
+  cardsImageRouter,
 };
