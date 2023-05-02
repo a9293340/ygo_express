c7177b76be98c54104f61104d845dd29a6302694
Eric Hong <f102041332@gmail.com>
2023-05-02 23:08:59 +0800

    [feature] cards fuzzy search

--- routes/backend/cards.js
+++ routes/backend/cards.js
@@ -1,75 +1,91 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
-	checkToken,
-	decryptRes,
-	fuzzySearch,
-} = require('../../config/tools/encryptNToken');
-const { limiter } = require('../../config/tools/rate-limiter');
+  checkToken,
+  decryptRes,
+  fuzzySearch,
+} = require("../../config/tools/encryptNToken");
+const { limiter } = require("../../config/tools/rate-limiter");
 const {
-	pAggregate,
-	canNotBeSameBeforeAdd,
-} = require('../../config/tools/postAction');
-const { articleEdit } = require('../../config/tools/articleApi');
+  pAggregate,
+  canNotBeSameBeforeAdd,
+} = require("../../config/tools/postAction");
+const { articleEdit } = require("../../config/tools/articleApi");
 
-router.post('/list', limiter, checkToken, async (req, res, next) => {
-	const { filter, limit, page } = decryptRes(req.body.data);
+router.post("/list", limiter, checkToken, async (req, res, next) => {
+  const { filter, limit, page } = decryptRes(req.body.data);
 
-	const { atk_t, atk_l, def_t, def_l, rarity, name, id, ...useful } = filter;
-	let target = {};
-	// atk
-	if (atk_l | atk_t) {
-		target['atk'] = {};
-		if (atk_l) target['atk']['$gte'] = atk_l;
-		if (atk_t) target['atk']['$lte'] = atk_t;
-	}
-	// def
-	if (def_l | def_t) {
-		target['def'] = {};
-		if (def_l) target['def']['$gte'] = def_l;
-		if (def_t) target['def']['$lte'] = def_t;
-	}
-	// rarity
-	if (rarity) {
-		target['rarity'] = {
-			$elemMatch: { $eq: rarity },
-		};
-	}
-	// id
-	if (id) target['id'] = fuzzySearch(id);
-	// _id number type attribute start product_information_type
-	if (useful) target = { ...target, ...useful };
+  const { atk_t, atk_l, def_t, def_l, rarity, name, effact, id, ...useful } =
+    filter;
+  let target = {};
+  // atk
+  if (atk_l || atk_t) {
+    target["atk"] = {};
+    if (atk_l) target["atk"]["$gte"] = atk_l;
+    if (atk_t) target["atk"]["$lte"] = atk_t;
+  }
+  // def
+  if (def_l || def_t) {
+    target["def"] = {};
+    if (def_l) target["def"]["$gte"] = def_l;
+    if (def_t) target["def"]["$lte"] = def_t;
+  }
+  // rarity
+  if (rarity) {
+    target["rarity"] = {
+      $elemMatch: { $eq: rarity },
+    };
+  }
+  // id
+  if (id) target["id"] = fuzzySearch(id);
+  // _id number type attribute start product_information_type
+  if (useful) target = { ...target, ...useful };
 
-	let aggregateFilter = [];
+  let aggregateFilter = [];
 
-	// name
-	if (name) {
-		aggregateFilter.push({
-			$search: {
-				index: 'cards',
-				text: {
-					query: name,
-					path: {
-						wildcard: '*',
-					},
-				},
-			},
-		});
-	}
-	aggregateFilter.push({ $match: target });
-	aggregateFilter.push({ $skip: page * limit });
-	aggregateFilter.push({ $limit: limit });
+  // name
+  if (name) {
+    aggregateFilter.push({
+      $search: {
+        index: "cards",
+        text: {
+          query: name,
+          path: {
+            wildcard: "*",
+          },
+        },
+      },
+    });
+  }
 
-	pAggregate(res, 'cards', aggregateFilter, { limit, page });
+  if (effact) {
+    aggregateFilter.push({
+      $search: {
+        index: "cards_effactg",
+        text: {
+          query: effact,
+          path: {
+            wildcard: "*",
+          },
+        },
+      },
+    });
+  }
+
+  aggregateFilter.push({ $match: target });
+  aggregateFilter.push({ $skip: page * limit });
+  aggregateFilter.push({ $limit: limit });
+
+  await pAggregate(res, "cards", aggregateFilter, { limit, page });
 });
 
-router.post('/add', limiter, checkToken, async (req, res, next) => {
-	let { token, tokenReq, ...useful } = decryptRes(req.body.data);
-	canNotBeSameBeforeAdd(res, next, 'cards', useful, 'id');
+router.post("/add", limiter, checkToken, async (req, res, next) => {
+  let { token, tokenReq, ...useful } = decryptRes(req.body.data);
+  await canNotBeSameBeforeAdd(res, next, "cards", useful, "id");
 });
 
-router.post('/edit', limiter, checkToken, async (req, res, next) => {
-	articleEdit(req, res, next, 'cards');
+router.post("/edit", limiter, checkToken, async (req, res, next) => {
+  await articleEdit(req, res, next, "cards");
 });
 
 module.exports = router;
