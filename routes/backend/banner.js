35f752d80e6d663f424c53333620ab585a7ea728
Eric Hong <f102041332@gmail.com>
2023-08-27 21:46:25 +0800

    [fixed]

--- routes/backend/banner.js
+++ routes/backend/banner.js
@@ -2,35 +2,44 @@ const express = require('express');
 const { limiter } = require('../../config/tools/rate-limiter');
 const { checkToken, decryptRes } = require('../../config/tools/encryptNToken');
 const { toISODate } = require('../../config/tools/toDate');
-const { pAdd, pList } = require('../../config/tools/postAction');
-const { articleEdit } = require('../../config/tools/articleApi');
+const { pAdd, pList, pEdit } = require('../../config/tools/postAction');
 const router = express.Router();
 
 router.post('/add', limiter, checkToken, (req, res, next) => {
-	const { token, tokenReq, ...use } = decryptRes(req.body.data);
-	use.date = toISODate(use.date);
-	pAdd(res, next, 'banner', use);
+  let { token, tokenReq, ...use } = decryptRes(req.body.data);
+  console.log(use);
+  use.date = new Date();
+  pAdd(res, next, 'banner', use);
 });
 
 router.post('/list', limiter, checkToken, (req, res, next) => {
-	const { filter, limit, page } = decryptRes(req.body.data);
-	const target =
-		filter.begin_date && filter.end_date
-			? {
-					date: {
-						$gte: toISODate(filter.begin_date),
-						$lte: toISODate(filter.end_date),
-					},
-			  }
-			: {};
-	pList(res, next, 'banner', target, true, {
-		limit,
-		page,
-	});
+  const { filter, limit, page } = decryptRes(req.body.data);
+  const target =
+    filter.begin_date && filter.end_date
+      ? {
+          date: {
+            $gte: toISODate(filter.begin_date),
+            $lte: toISODate(filter.end_date),
+          },
+        }
+      : {};
+  pList(res, next, 'banner', target, true, {
+    limit,
+    page,
+  });
 });
 
 router.post('/edit', limiter, checkToken, (req, res, next) => {
-	articleEdit(req, res, next, 'banner');
+  let { token, tokenReq, _id, ...use } = decryptRes(req.body.data);
+  use.date = new Date();
+  if (!_id) next(10004);
+  else {
+    try {
+      pEdit(res, next, 'banner', use, _id);
+    } catch (e) {
+      next(10003);
+    }
+  }
 });
 
 module.exports = router;
