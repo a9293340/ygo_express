b7adac78993a30f777532555e77eda79fcff2a2d
Eric <f102041332@gmail.com>
2023-11-01 09:20:21 +0800

    [fixed] makeAutorName if no author _id continue for loop

--- config/tools/makeAutorName.js
+++ config/tools/makeAutorName.js
@@ -1,29 +1,30 @@
-const { MongooseCRUD } = require('../MongoDb/Api');
+const { MongooseCRUD } = require("../MongoDb/Api");
 
-const makeAuthorName = async arr => {
-  if (Array.isArray(arr)) {
-    const admin_ids = arr.map(x => x.admin_id);
-    const authors = await MongooseCRUD(
-      'R',
-      'admin',
-      { _id: admin_ids },
-      {},
-      {
-        _id: 1,
-        name: 1,
-      },
-    );
+const makeAuthorName = async (arr) => {
+	if (Array.isArray(arr)) {
+		const admin_ids = arr.map((x) => x.admin_id);
+		const authors = await MongooseCRUD(
+			"R",
+			"admin",
+			{ _id: admin_ids },
+			{},
+			{
+				_id: 1,
+				name: 1,
+			}
+		);
 
-    let res = JSON.parse(JSON.stringify(arr));
-    for (let i = 0; i < res.length; i++) {
-      const tar = res[i].admin_id;
-      const name = authors.find(x => x._id.toString() === tar).name;
-      res[i].author_name = name;
-    }
-    return res;
-  } else return arr;
+		let res = JSON.parse(JSON.stringify(arr));
+		for (let i = 0; i < res.length; i++) {
+			const tar = res[i].admin_id;
+			if (!authors.find((x) => x._id.toString() === tar)) continue;
+			const name = authors.find((x) => x._id.toString() === tar).name;
+			res[i].author_name = name;
+		}
+		return res;
+	} else return arr;
 };
 
 module.exports = {
-  makeAuthorName,
+	makeAuthorName,
 };
