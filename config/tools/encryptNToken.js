a59c8a35a4751e608e8f1d83987051c156835f6e
Eric <f102041332@gmail.com>
2023-11-16 11:12:09 +0800

    [fixed] deckList api issue

--- config/tools/encryptNToken.js
+++ config/tools/encryptNToken.js
@@ -5,7 +5,7 @@ const frontend = require("../db/frontend.json");
 
 const checkFrontEndPath = (req) => {
 	const check = frontend[req.path.replace("/", "")];
-	console.log(req.path);
+	console.log(check, req.baseUrl);
 	return check ? !!check.find((el) => req.baseUrl.indexOf(el) !== -1) : false;
 };
 
