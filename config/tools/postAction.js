5cfaa0409acbfbcc360888a18cf688d010987864
Eric <f102041332@gmail.com>
2023-11-17 11:19:37 +0800

    [fixed] deck api limit=1

--- config/tools/postAction.js
+++ config/tools/postAction.js
@@ -1,168 +1,187 @@
-const { MongooseCRUD } = require('../../config/MongoDb/Api');
-const { encryptRes } = require('./encryptNToken');
-const { makeAuthorName } = require('./makeAutorName');
-const { parsingPermission2Obj, makePermission } = require('./permission');
-const { makeArticleDateArr } = require('./toDate');
+const { MongooseCRUD } = require("../../config/MongoDb/Api");
+const { encryptRes } = require("./encryptNToken");
+const { makeAuthorName } = require("./makeAutorName");
+const { parsingPermission2Obj, makePermission } = require("./permission");
+const { makeArticleDateArr } = require("./toDate");
 
 const pList = (
-  res,
-  next,
-  modelName,
-  target,
-  hasDateFormat = false,
-  hasPage = false,
-  projection = {},
+	res,
+	next,
+	modelName,
+	target,
+	hasDateFormat = false,
+	hasPage = false,
+	projection = {}
 ) => {
-  const hasLimitPage = hasPage && Number.isInteger(hasPage.limit) && Number.isInteger(hasPage.page);
-  let option = hasLimitPage
-    ? {
-        limit: hasPage.limit,
-        skip: hasPage.page * hasPage.limit,
-      }
-    : {};
-  if (hasDateFormat) {
-    const keys = Object.keys(target);
-    let key = '';
-    if (keys.find(el => el === 'date')) key = 'date';
-    else if (keys.find(el => el === 'publish_date')) key = 'publish_date';
-    else if (keys.find(el => el === 'create_date')) key = 'create_date';
-    // banner
-    else if (!keys.length) key = 'date';
-    if (key !== '') {
-      option.sort = {};
-      option.sort[key] = -1;
-    }
-  }
-  console.log(option);
-  // console.log(modelName, target, option);
-  MongooseCRUD('R', modelName, target, option, projection).then(async (arr, err) => {
-    console.log(arr);
-    if (err) next(err);
-    if (!arr.length)
-      res.status(200).json({
-        error_code: 0,
-        data: encryptRes(
-          hasPage
-            ? {
-                total: 0,
-                list: arr,
-              }
-            : {
-                list: arr,
-              },
-        ),
-      });
-    else if (
-      hasLimitPage &&
-      (!Number.isInteger(hasPage.limit) || !Number.isInteger(hasPage.page))
-    ) {
-      next(10004);
-    } else {
-      // 轉換日期
-      if (hasDateFormat) arr = makeArticleDateArr(arr);
-      // 文章新增author_name
-      if (arr[0]['admin_id'] && modelName !== 'decks') arr = await makeAuthorName(arr);
-      // permission 需轉換成object格式
-      if (modelName === 'permission') {
-        for (let i = 0; i < arr.length; i++) {
-          arr[i].permission = parsingPermission2Obj(arr[i].permission);
-        }
-      }
-      const count = hasLimitPage ? await MongooseCRUD('COUNT', modelName, target) : 0;
-      console.log(arr);
-      res.status(200).json({
-        error_code: 0,
-        data: encryptRes(
-          hasLimitPage
-            ? {
-                total: count,
-                list: arr,
-              }
-            : {
-                list: arr,
-              },
-        ),
-      });
-    }
-  });
+	const hasLimitPage =
+		hasPage &&
+		Number.isInteger(hasPage.limit) &&
+		Number.isInteger(hasPage.page);
+	let option = hasLimitPage
+		? {
+				limit: hasPage.limit,
+				skip: hasPage.page * hasPage.limit,
+		  }
+		: {};
+	if (hasDateFormat) {
+		const keys = Object.keys(target);
+		let key = "";
+		if (keys.find((el) => el === "date")) key = "date";
+		else if (keys.find((el) => el === "publish_date")) key = "publish_date";
+		else if (keys.find((el) => el === "create_date")) key = "create_date";
+		// banner
+		else if (!keys.length) key = "date";
+		if (key !== "") {
+			option.sort = {};
+			option.sort[key] = -1;
+		}
+	}
+	console.log(option);
+	// console.log(modelName, target, option);
+	MongooseCRUD("R", modelName, target, option, projection).then(
+		async (arr, err) => {
+			console.log(arr);
+			if (err) next(err);
+			if (!arr.length)
+				res.status(200).json({
+					error_code: 0,
+					data: encryptRes(
+						hasPage
+							? {
+									total: 0,
+									list: arr,
+							  }
+							: {
+									list: arr,
+							  }
+					),
+				});
+			else if (
+				hasLimitPage &&
+				(!Number.isInteger(hasPage.limit) || !Number.isInteger(hasPage.page))
+			) {
+				next(10004);
+			} else {
+				// 轉換日期
+				if (hasDateFormat) arr = makeArticleDateArr(arr);
+				// 文章新增author_name
+				if (arr[0]["admin_id"] && modelName !== "decks")
+					arr = await makeAuthorName(arr);
+				// permission 需轉換成object格式
+				if (modelName === "permission") {
+					for (let i = 0; i < arr.length; i++) {
+						arr[i].permission = parsingPermission2Obj(arr[i].permission);
+					}
+				}
+				const count = hasLimitPage
+					? await MongooseCRUD("COUNT", modelName, target)
+					: 0;
+				console.log(arr);
+				res.status(200).json({
+					error_code: 0,
+					data: encryptRes(
+						hasLimitPage
+							? {
+									total: count,
+									list: arr,
+							  }
+							: {
+									list: arr,
+							  }
+					),
+				});
+			}
+		}
+	);
 };
 
 const pAdd = (res, next, modelName, use) => {
-  MongooseCRUD('C', modelName, use).then((arr, err) => {
-    if (err) next(err);
-    else {
-      res.status(200).json({
-        error_code: 0,
-        data: encryptRes({}),
-      });
-    }
-  });
+	MongooseCRUD("C", modelName, use).then((arr, err) => {
+		if (err) next(err);
+		else {
+			res.status(200).json({
+				error_code: 0,
+				data: encryptRes({}),
+			});
+		}
+	});
 };
 
 const pEdit = async (res, next, modelName, use, _id) => {
-  if (modelName === 'permission') use.permission = makePermission(use.permission);
-  try {
-    let arr = await MongooseCRUD('Uo', modelName, { _id }, use);
-    res.status(200).json({
-      error_code: !arr['matchedCount'] ? 10007 : 0,
-      data: encryptRes({}),
-    });
-  } catch (error) {
-    next(10003);
-  }
+	if (modelName === "permission")
+		use.permission = makePermission(use.permission);
+	try {
+		let arr = await MongooseCRUD("Uo", modelName, { _id }, use);
+		res.status(200).json({
+			error_code: !arr["matchedCount"] ? 10007 : 0,
+			data: encryptRes({}),
+		});
+	} catch (error) {
+		next(10003);
+	}
 };
 
 const pAggregate = async (res, modelName, target, hasPage) => {
-  MongooseCRUD('A', modelName, target, {}, {}).then(async (arr, err) => {
-    console.log(arr);
-    if (err) next(err);
-    else if (!arr.length) {
-      res.status(200).json({
-        error_code: 0,
-        data: encryptRes({
-          total: 0,
-          list: [],
-        }),
-      });
-    } else if (hasPage && (!Number.isInteger(hasPage.limit) || !Number.isInteger(hasPage.page))) {
-      next(10004);
-    } else {
-      let lastFilter = target.filter(
-        el => Object.keys(el)[0] === '$match' || Object.keys(el)[0] === '$search',
-      );
-      // Add Count
-      lastFilter.push({
-        $count: 'count',
-      });
+	MongooseCRUD("A", modelName, target, {}, {}).then(async (arr, err) => {
+		// console.log(arr);
+		if (err) next(err);
+		else if (!arr.length) {
+			res.status(200).json({
+				error_code: 0,
+				data: encryptRes({
+					total: 0,
+					list: [],
+				}),
+			});
+		} else if (
+			hasPage &&
+			(!Number.isInteger(hasPage.limit) || !Number.isInteger(hasPage.page))
+		) {
+			next(10004);
+		} else {
+			let lastFilter = target.filter(
+				(el) =>
+					Object.keys(el)[0] === "$match" || Object.keys(el)[0] === "$search"
+			);
+			// Add Count
+			lastFilter.push({
+				$count: "count",
+			});
 
-      console.log(await MongooseCRUD('A', modelName, lastFilter));
-      const total = (await MongooseCRUD('A', modelName, lastFilter))[0].count;
-      res.status(200).json({
-        error_code: 0,
-        data: encryptRes({
-          total,
-          list: arr,
-        }),
-      });
-    }
-  });
+			// console.log(await MongooseCRUD('A', modelName, lastFilter));
+			const total = (await MongooseCRUD("A", modelName, lastFilter))[0].count;
+			res.status(200).json({
+				error_code: 0,
+				data: encryptRes({
+					total,
+					list: arr,
+				}),
+			});
+		}
+	});
 };
 
-const canNotBeSameBeforeAdd = async (res, next, modelName, useful, checkKey) => {
-  if (!useful[checkKey]) next(10004);
-  else {
-    let obj = {};
-    obj[checkKey] = useful[checkKey];
-    const arr = await MongooseCRUD('R', modelName, obj);
-    if (arr.length) next(10012);
-    else pAdd(res, next, modelName, useful);
-  }
+const canNotBeSameBeforeAdd = async (
+	res,
+	next,
+	modelName,
+	useful,
+	checkKey
+) => {
+	if (!useful[checkKey]) next(10004);
+	else {
+		let obj = {};
+		obj[checkKey] = useful[checkKey];
+		const arr = await MongooseCRUD("R", modelName, obj);
+		if (arr.length) next(10012);
+		else pAdd(res, next, modelName, useful);
+	}
 };
 
 module.exports = {
-  pList,
-  pAdd,
-  pEdit,
-  canNotBeSameBeforeAdd,
-  pAggregate,
+	pList,
+	pAdd,
+	pEdit,
+	canNotBeSameBeforeAdd,
+	pAggregate,
 };
--- routes/frontend/deck.js
+++ routes/frontend/deck.js
@@ -1,100 +1,116 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { MongooseCRUD } = require('../../config/MongoDb/Api');
+const { MongooseCRUD } = require("../../config/MongoDb/Api");
 const {
-  checkToken,
-  decryptRes,
-  encryptRes,
-  fuzzySearch,
-} = require('../../config/tools/encryptNToken');
-const { limiter } = require('../../config/tools/rate-limiter');
-const { pList, pAdd } = require('../../config/tools/postAction');
-const { articleEdit } = require('../../config/tools/articleApi');
-const { toISODate } = require('../../config/tools/toDate');
+	checkToken,
+	decryptRes,
+	encryptRes,
+	fuzzySearch,
+} = require("../../config/tools/encryptNToken");
+const { limiter } = require("../../config/tools/rate-limiter");
+const { pList, pAdd } = require("../../config/tools/postAction");
+const { articleEdit } = require("../../config/tools/articleApi");
+const { toISODate } = require("../../config/tools/toDate");
 
-router.post('/deckList', limiter, checkToken, async (req, res, next) => {
-  const { page, limit, filter } = decryptRes(req.body.data);
-  console.log(page, limit, filter);
-  try {
-    if (limit > 1) {
-      let target = {};
-      const { title, begin_date, end_date, ...other } = filter;
-      if (title) target.title = fuzzySearch(title);
-      if (begin_date && end_date)
-        target.create_date = {
-          $gte: toISODate(begin_date),
-          $lte: toISODate(end_date),
-        };
-      if (other) target = { ...target, ...other };
-      pList(
-        res,
-        next,
-        'decks',
-        target,
-        begin_date && end_date,
-        { limit, page },
-        { main_deck: 0, extra_deck: 0, side_deck: 0 },
-      );
-    } else {
-      let deck = await MongooseCRUD('R', 'decks', filter, { limit, page });
-      const getCardsInfo = async (deck, standard) =>
-        (await MongooseCRUD('R', 'cards', { id: deck })).map(el => {
-          el.rarity = el.rarity.find(x => x === standard.card_rarity);
-          return el;
-        });
-      if (deck.main_deck.length)
-        deck.main_deck = await getCardsInfo(
-          deck.main_deck.map(x => x.card_id),
-          deck.main_deck,
-        );
-      if (deck.extra_deck.length)
-        deck.extra_deck = await getCardsInfo(
-          deck.main_deck.map(x => x.card_id),
-          deck.main_deck,
-        );
-      if (deck.side_deck.length)
-        deck.side_deck = await getCardsInfo(
-          deck.main_deck.map(x => x.card_id),
-          deck.main_deck,
-        );
-
-      res.status(200).json({
-        error_code: 0,
-        data: encryptRes({
-          total: 1,
-          list: deck,
-        }),
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    next(10003);
-  }
+router.post("/deckList", limiter, checkToken, async (req, res, next) => {
+	const { page, limit, filter } = decryptRes(req.body.data);
+	console.log(page, limit, filter);
+	try {
+		if (limit > 1) {
+			let target = {};
+			const { title, begin_date, end_date, ...other } = filter;
+			if (title) target.title = fuzzySearch(title);
+			if (begin_date && end_date)
+				target.create_date = {
+					$gte: toISODate(begin_date),
+					$lte: toISODate(end_date),
+				};
+			if (other) target = { ...target, ...other };
+			pList(
+				res,
+				next,
+				"decks",
+				target,
+				begin_date && end_date,
+				{ limit, page },
+				{ main_deck: 0, extra_deck: 0, side_deck: 0 }
+			);
+		} else {
+			let deck = (await MongooseCRUD("R", "decks", filter, { limit, page }))[0];
+			console.log(deck);
+			const getCardsInfo = async (_id, deck) => {
+				const info = await MongooseCRUD(
+					"R",
+					"cards",
+					{ _id },
+					{},
+					{ price_info: 0 }
+				);
+				return deck.map((item) => {
+					let final = info.find((x) => x._id.toString() === item.card_id);
+					return {
+						card_id: final._id,
+						card_rarity: item.card_rarity,
+						card_number: final.number,
+						card_num_id: final.id,
+					};
+				});
+			};
+			if (deck.main_deck.length) {
+				deck.main_deck = await getCardsInfo(
+					deck.main_deck.map((x) => x.card_id),
+					deck.main_deck
+				);
+			}
+			if (deck.extra_deck.length) {
+				deck.extra_deck = await getCardsInfo(
+					deck.extra_deck.map((x) => x.card_id),
+					deck.extra_deck
+				);
+			}
+			if (deck.side_deck.length) {
+				deck.side_deck = await getCardsInfo(
+					deck.side_deck.map((x) => x.card_id),
+					deck.side_deck
+				);
+			}
+			res.status(200).json({
+				error_code: 0,
+				data: encryptRes({
+					total: 1,
+					list: [deck],
+				}),
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		next(10003);
+	}
 });
 
-router.post('/add', limiter, checkToken, async (req, res, next) => {
-  const { token, tokenReq, ...filter } = decryptRes(req.body.data);
-  if (!filter || typeof filter !== 'object') next(10003);
-  else pAdd(res, next, 'decks', filter);
+router.post("/add", limiter, checkToken, async (req, res, next) => {
+	const { token, tokenReq, ...filter } = decryptRes(req.body.data);
+	if (!filter || typeof filter !== "object") next(10003);
+	else pAdd(res, next, "decks", filter);
 });
 
-router.post('/edit', limiter, checkToken, async (req, res, next) => {
-  await articleEdit(req, res, next, 'decks');
+router.post("/edit", limiter, checkToken, async (req, res, next) => {
+	await articleEdit(req, res, next, "decks");
 });
 
-router.post('/delete', limiter, checkToken, async (req, res, next) => {
-  const { token, tokenReq, ...filter } = decryptRes(req.body.data);
-  try {
-    let arr = await MongooseCRUD('D', 'decks', filter);
-    res.status(200).json({
-      error_code: !arr['matchedCount'] ? 10007 : 0,
-      data: encryptRes({}),
-    });
-  } catch (error) {
-    console.log(error);
-    next(10003);
-  }
+router.post("/delete", limiter, checkToken, async (req, res, next) => {
+	const { token, tokenReq, ...filter } = decryptRes(req.body.data);
+	try {
+		let arr = await MongooseCRUD("D", "decks", filter);
+		res.status(200).json({
+			error_code: !arr["matchedCount"] ? 10007 : 0,
+			data: encryptRes({}),
+		});
+	} catch (error) {
+		console.log(error);
+		next(10003);
+	}
 });
 
 module.exports = router;
