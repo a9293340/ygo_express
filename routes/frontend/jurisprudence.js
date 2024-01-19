052b01ed34e1dbd785362fbd71ab5e413d97e1b8
Eric <f102041332@gmail.com>
2024-01-19 17:09:08 +0800

    [fixed] jurisprudence list res.body修正

--- routes/frontend/jurisprudence.js
+++ routes/frontend/jurisprudence.js
@@ -5,21 +5,17 @@ const { limiter } = require("../../config/tools/rate-limiter");
 const { MongooseCRUD } = require("../../config/MongoDb/Api");
 
 router.post("/list", limiter, async (req, res, next) => {
-	const { filter } = decryptRes(req.body.data);
-	if (!filter) next(10004);
+	const { number } = decryptRes(req.body.data);
+	if (!number) next(10004);
 	else {
-		const { number } = filter;
-		if (!number) next(10004);
-		else {
-			const data = await MongooseCRUD("R", "jurisprudence", { number });
-			res.status(200).json({
-				error_code: 0,
-				data: encryptRes({
-					total: data.length,
-					list: data,
-				}),
-			});
-		}
+		const data = await MongooseCRUD("R", "jurisprudence", { number });
+		res.status(200).json({
+			error_code: 0,
+			data: encryptRes({
+				total: data.length,
+				list: data,
+			}),
+		});
 	}
 });
 
